c7dbcc7b3b4a41106dbfaeed5ae9090164861cc7
Tomba <tomba@bat.org>
2008-07-30 21:03:52 +0300

    foo

--- scripts/lib/batclient.py
+++ scripts/lib/batclient.py
@@ -36,6 +36,15 @@ def send(str):
 	Net.SendLine(str)
 	
 def write(str, *args):
+	"""Writes text to screen.
+	
+	str - string to be printed
+	args - formatting args
+	
+	Examples:
+    write("foobar")
+    write("foo {0} bar {1}", 123, "pla")
+	"""
 	Console.WriteLine(str, *args)
 
 def isconnected():
